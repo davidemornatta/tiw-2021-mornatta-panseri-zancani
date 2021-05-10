241a954e743c84b69f2e2e15f66aaba5e7561db1
lea <lea.zancani@mail.polimi.it>
2021-05-10 18:27:56 +0200

    feat: implement Product bean

---
+++ src/main/java/it/polimi/tiw/group83/tiw_2021_mornatta_panseri_zancani/beans/Product.java
@@ -0,0 +1,40 @@
+package it.polimi.tiw.group83.tiw_2021_mornatta_panseri_zancani.beans;
+
+public class Product {
+    private int code;
+    private String name;
+    private String category;
+    private String description;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
