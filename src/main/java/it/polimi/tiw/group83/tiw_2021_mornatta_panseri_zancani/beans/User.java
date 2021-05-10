6ca64c143a398ff44419035497a6b86fbc91cec7
Leonardo Panseri <leonardo.panseri@gmail.com>
2021-05-10 18:17:59 +0200

    fix: remove unused variable in user

--- src/main/java/it/polimi/tiw/group83/tiw_2021_mornatta_panseri_zancani/beans/User.java
+++ src/main/java/it/polimi/tiw/group83/tiw_2021_mornatta_panseri_zancani/beans/User.java
@@ -2,7 +2,7 @@ package it.polimi.tiw.group83.tiw_2021_mornatta_panseri_zancani.beans;
 
 public class User {
     private int id;
-    private String username;
+
     private String name;
     private String surname;
 
@@ -12,14 +12,6 @@ public class User {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
     }
 
     public String getName() {
